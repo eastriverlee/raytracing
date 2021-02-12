7c760dc6ea3e130ec2a1b1c39342b3f75f41a016
eastriverlee <dan0720@naver.com>
2021-02-12 15:42:44 +0900

    drop malloc

--- vec3.h
+++ vec3.h
@@ -5,6 +5,9 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+# define TRUE	1
+# define FALSE	0
+
 typedef struct	vec3
 {
 	double x;
@@ -12,56 +15,56 @@ typedef struct	vec3
 	double z;
 }				vec3;
 
-vec3 *init(vec3 *v, double e1, double e2, double e3)
+vec3 vector(double e1, double e2, double e3)
 {
-	v = malloc(sizeof(vec3));
-	v->x = e1;
-	v->y = e2;
-	v->z = e3;
+	vec3 v;
+
+	v.x = e1;
+	v.y = e2;
+	v.z = e3;
 	return (v);
 }
 
-vec3 *negative(vec3 v)
+vec3 negative(vec3 v)
 {
-	vec3 *_v;
-
-	return (init(_v, -v.x, -v.y, -v.z));
+	return (vector(-v.x, -v.y, -v.z));
 }
 
-void multiply(vec3 *v, double t)
+vec3 multiply(vec3 *v, double t, int ismutating)
 {
-	v->x *= t;
-	v->y *= t;
-	v->z *= t;
+	if (ismutating)
+	{
+		v->x *= t;
+		v->y *= t;
+		v->z *= t;
+		return (*v);
+	}
+	return (vector(v->x*t, v->y*t, v->z*t));
 }
 
-void divide(vec3 *v, double t)
+vec3 divide(vec3 *v, double t, int ismutating)
 {
-	multiply(v, 1/t);
+	return (multiply(v, 1/t, ismutating));
 }
 
-double length_squared(vec3 *v)
+double length_squared(vec3 v)
 {
-	return (v->x*v->x + v->y*v->y + v->z*v->z);
+	return (v.x*v.x + v.y*v.y + v.z*v.z);
 }
 
-double length(vec3 *v)
+double length(vec3 v)
 {
 	return (sqrt(length_squared(v)));
 }
 
-vec3 *plus(vec3 u, vec3 v)
+vec3 plus(vec3 u, vec3 v)
 {
-	vec3 *_v;
-	
-	return (init(_v, u.x + v.x, u.y + v.y, u.z + v.z));
+	return (vector(u.x + v.x, u.y + v.y, u.z + v.z));
 }
 
-vec3 *minus(vec3 u, vec3 v)
+vec3 minus(vec3 u, vec3 v)
 {
-	vec3 *_v;
-	
-	return (init(_v, u.x - v.x, u.y - v.y, u.z - v.z));
+	return (vector(u.x - v.x, u.y - v.y, u.z - v.z));
 }
 
 double dot(vec3 u, vec3 v)
@@ -71,30 +74,16 @@ double dot(vec3 u, vec3 v)
 			+u.z * v.z);
 }
 
-vec3 *cross(vec3 u, vec3 v)
+vec3 cross(vec3 u, vec3 v)
 {
-	vec3 *_v;
-
-	return (init(_v,
-					u.y * v.z - u.z * v.y,
+	return (vector(u.y * v.z - u.z * v.y,
 					u.z * v.x - u.x * v.z,
 					u.x * v.y - u.y * v.x));
 }
 
-vec3 *duplicate(vec3 v)
+vec3 unit_vector(vec3 v)
 {
-	vec3 *_v;
-
-	return (init(_v, v.x, v.y, v.z));
-}
-
-vec3 *unit_vector(vec3 v)
-{
-	vec3 *_v;
-
-	_v = duplicate(v);
-	divide(_v, length(_v));
-	return (_v);
+	return (divide(&v, length(v), FALSE));
 }
 
 void update(vec3 *v, double e1, double e2, double e3)
