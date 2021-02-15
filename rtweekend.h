73c2aa346e07eab35d034706a0edcb15963c9da3
eastriverlee <dan0720@naver.com>
2021-02-15 22:35:53 +0900

    listing26

--- rtweekend.h
+++ rtweekend.h
@@ -2,6 +2,7 @@
 # define RTWEEKEND_H
 
 # include <math.h>
+# include <stdlib.h>
 
 const double infinity = INFINITY;
 const double pi = M_PI;
@@ -9,6 +10,16 @@ const double pi = M_PI;
 double degrees_to_radians(double degrees)
 {
 	return (degrees * pi / 180.0);
+}
+
+double random_double()
+{
+	return (rand() / (RAND_MAX + 1.0));
+}
+
+double random_double_(double min, double max)
+{
+	return (min + (max-min)*random_double());
 }
 
 # include "ray.h"
