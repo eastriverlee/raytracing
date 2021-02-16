a3063211da3a406bc2ab6db2dfa3b72fabdccbc7
eastriverlee <dan0720@naver.com>
2021-02-16 16:10:16 +0900

    listing36

--- color.h
+++ color.h
@@ -11,9 +11,9 @@ void write_color(color pixel_color, int samples_per_pixel)
 	double b = pixel_color.z;
 
 	double scale = 1.0 / samples_per_pixel;
-	r *= scale;
-	g *= scale;
-	b *= scale;
+	r = sqrt(scale * r);
+	g = sqrt(scale * g);
+	b = sqrt(scale * b);
 
 	int ir = (int)(256 * clamp(r, 0.0, 0.999));
 	int ig = (int)(256 * clamp(g, 0.0, 0.999));
