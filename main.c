4e2443e9f368c570782e11baa3f714ec23c10cf6
eastriverlee <dan0720@naver.com>
2021-02-19 00:53:24 +0900

    listing70

--- main.c
+++ main.c
@@ -6,6 +6,58 @@
 #include "sphere.h"
 #include "camera.h"
 
+#define make_lambertian(c) material_(lambertian, c, FALSE, FALSE); 
+#define make_metal(c, f) material_(metal, c, f, FALSE); 
+#define make_dielectric(i) material_(dielectric, color_(1,1,1), FALSE, i); 
+
+list *random_scene()
+{
+	color albedo;
+	list *world = NULL;
+	int a, b;
+
+	material ground_material = make_lambertian(color_(0.5, 0.5, 0.5));
+	push(&world, list_(sphere_(point3_(0,-1000,0), 1000, ground_material)));
+
+	for (a = -11; a < 11; a++)
+		for (b = -11; b < 11; b++)
+		{
+			double choose_mat = random_double();
+			point3 center = point3_(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+			if ((length(subtract(center, point3_(4, 0.2, 0))) > 0.9))
+			{
+				material sphere_material;
+
+				if (choose_mat < 0.8)
+				{
+					albedo = multiply__(vec3_random(), vec3_random());
+					sphere_material = make_lambertian(albedo);
+				}
+				else if (choose_mat < 0.95)
+				{
+					albedo = vec3_random_(0.5, 1);
+					double fuzz = random_double_(0, 0.5);
+					sphere_material = make_metal(albedo, fuzz);
+				}
+				else
+					sphere_material = make_dielectric(1.5);
+				push(&world, list_(sphere_(center, 0.2, sphere_material)));
+			}
+		}
+
+	material material1 = make_dielectric(1.5);
+	push(&world, list_(sphere_(point3_(0, 1, 0), 1, material1)));
+
+	material material2 = make_lambertian(color_(0.4, 0.2, 0.1));
+	push(&world, list_(sphere_(point3_(-4, 1, 0), 1, material2)));
+
+	material material3 = make_metal(color_(0.7, 0.6, 0.5), 0);
+	push(&world, list_(sphere_(point3_(4, 1, 0), 1, material3)));
+
+	return (world);
+}
+
 color ray_color(ray *r, list *world, int depth)
 {
 	hit_record rec;
@@ -30,31 +82,20 @@ color ray_color(ray *r, list *world, int depth)
 
 int	main()
 {
-	const float aspect_ratio = 16.0 / 9.0;
-	const int image_width = 400;
+	const float aspect_ratio = 1 / 1;
+	const int image_width = 480;
 	const int image_height = (int)(image_width / aspect_ratio);
-	const int samples_per_pixel = 100;
+	const int samples_per_pixel = 50;
 	const int max_depth = 50;
 	int i, j, s;
 
-	list *world;
+	list *world = random_scene();
 
-	material material_ground = material_(lambertian, color_(0.8, 0.8, 0.0), FALSE, FALSE);
-	material material_center = material_(lambertian, color_(0.1, 0.2, 0.5), FALSE, FALSE);
-	material material_left = material_(dielectric, color_(1.0, 1.0, 1.0), FALSE, 1.5);
-	material material_right = material_(metal, color_(0.8, 0.6, 0.2), 0.0, FALSE);
-
-	push(&world, list_(sphere_(point3_(0.0,-100.5,-1.0), 100, material_ground)));
-	push(&world, list_(sphere_(point3_(0.0,0.0,-1.0), 0.5, material_center)));
-	push(&world, list_(sphere_(point3_(-1.0,0.0,-1.0), 0.5, material_left)));
-	push(&world, list_(sphere_(point3_(-1.0,0.0,-1.0), -0.45, material_left)));
-	push(&world, list_(sphere_(point3_(1.0,0.0,-1.0), 0.5, material_right)));
-
-	point3 lookfrom = point3_(3,3,2);
-	point3 lookat = point3_(0,0,-1);
+	point3 lookfrom = point3_(13,2,3);
+	point3 lookat = point3_(0,0,0);
 	vec3 vup = vec3_(0,1,0);
-	double dist_to_focus = length(subtract(lookfrom, lookat));
-	double aperture = 2.0;
+	double dist_to_focus = 10;
+	double aperture = 0.1;
 
 	camera cam = camera_(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
